ae245de9d4a6f86ed0047875dab90c2534047e16
Olva <otimorry@gmail.com>
2016-11-23 22:23:05 -0500

    TDG Prototype

---
+++ TDG/desktop/src/proto/tdg/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package proto.tdg.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import proto.tdg.game.Launcher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
